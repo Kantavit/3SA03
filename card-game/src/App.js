6e22d0e73ce19c6f49b52367cc5dd06d7c952752
kantavit <krite-18354@hotmail.com>
2020-08-13 14:34:18 +0700

    commit3 add rules, description, fill background

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -8,6 +8,10 @@ var word = wordBundle[Math.floor(Math.random() * wordBundle.length)];
 function App() {
   return (
    <div className="App">
+     <h1>Rules</h1>
+     <h2>If you guessed it right you win the game.</h2>
+     <h2>Guess the word below ! (F5 to guess new word)</h2>
+
      <WordCard value={word}/>
    </div>
   );
