768675c05c85375d2135c594d82774ff1263af68
kantavit <krite-18354@hotmail.com>
2020-08-13 17:21:46 +0700

    commit5 add next word button

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -5,17 +5,24 @@ import WordCard from './WordCard';
 var wordBundle = ["hello", "card", "react", "word", "yarn", "nodejs"];
 var word = wordBundle[Math.floor(Math.random() * wordBundle.length)];
 
+const nextWord = () => {
+  window.location.reload(false);
+}
+
 function App() {
   return (
    <div className="App">
      <h1>Rules</h1>
      <h2>If you guessed it right you win the game.</h2>
-     <h2>Guess the word below ! (F5 to guess new word)</h2>
-     
+     <h2>
+        Guess the word below ! or <button className="button" onClick={nextWord}>Next word</button>
+     </h2>
+
      <WordCard value={word}/>
 
      <h2>Status:</h2>
      <h2 id="result"></h2>
+
    </div>
   );
  }
